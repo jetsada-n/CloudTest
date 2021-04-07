201458553dae70edd4d4938ddbf407ae7e79d8e1
jetsada-n <79188370+jetsada-n@users.noreply.github.com>
2021-04-07 15:42:18 +0700

    Update hellotest.py

--- hellotest.py
+++ hellotest.py
@@ -1,18 +1,10 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask
+app = Flask (__name__)
 
-@app.route('/')
-def home():
-    return render_template("home.html")
-
-@app.route("/John")
-def John():
-    return "Hello John."
-
-@app.route('/aboutUs')
-def aboutUs():
-    return render_template("aboutUs.html")
+@app.route("/")
+def hello():
+    return "เจษฎา นิ่มศิริ 6006021621017"
 
 if __name__ == '__main__':
-    app.dbbug = True
-    app.run(host='0.0.0.0', port=8000)
+
+    app.run(host='0.0.0.0',port=80)
